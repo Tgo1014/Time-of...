de60587f13854290968fbdf59198c20b04e5d7fc
Tiago Araujo de Souza <taraujodesouza@gmail.com>
2022-11-20 19:40:09 +0100

    feat: changes to home screen

--- presentation/src/main/java/tgo1014/timeof/presentation/features/home/HomeScreen.kt
+++ presentation/src/main/java/tgo1014/timeof/presentation/features/home/HomeScreen.kt
@@ -1,8 +1,10 @@
 package tgo1014.timeof.presentation.features.home
 
-import android.content.res.Configuration
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -10,16 +12,26 @@ import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import tgo1014.timeof.presentation.R
 import tgo1014.timeof.presentation.models.WhateverPresentation
+import tgo1014.timeof.presentation.ui.composables.DefaultPreview
 import tgo1014.timeof.presentation.ui.theme.TimeOfTheme
 import kotlin.time.Duration.Companion.seconds
 
@@ -40,32 +52,52 @@ fun HomeScreen(
 private fun HomeScreen(
     whateverList: List<WhateverPresentation>,
     onCreateWhatever: (String) -> Unit = {},
-) = Scaffold(
-    topBar = {},
-    content = {
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it)
-        ) {
-            items(whateverList) { whatever ->
-                Text(text = whatever.toString())
+) {
+    val topAppBarScrollState = rememberTopAppBarState()
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(topAppBarScrollState)
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            LargeTopAppBar(
+                title = {
+                    Row {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_clock),
+                            contentDescription = null,
+                            modifier = Modifier.align(CenterVertically)
+                        )
+                        Spacer(Modifier.size(8.dp))
+                        Text(
+                            text = stringResource(R.string.app_name),
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+        content = {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it)
+                    .nestedScroll(scrollBehavior.nestedScrollConnection),
+            ) {
+                items(whateverList) { whatever ->
+                    Text(text = whatever.toString())
+                }
             }
-        }
-    },
-    floatingActionButton = {
-        FloatingActionButton(onClick = { onCreateWhatever("FooBar") }) {
-            Icon(imageVector = Icons.Default.Add, contentDescription = null)
-        }
-    },
-    modifier = Modifier.fillMaxSize()
-)
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { onCreateWhatever("FooBar") }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        },
+    )
+}
 
 @Composable
-@Preview
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES or Configuration.UI_MODE_TYPE_NORMAL
-)
+@DefaultPreview
 private fun HomeScreenPreview() = TimeOfTheme {
     HomeScreen(List(5) { WhateverPresentation("teasdas", 1.seconds) })
 }
